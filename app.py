1611cc1d5b8af06c59823501c08597e174bb5ec0
gmaharsh <gmaharsh@yahoo.com>
2019-06-11 22:38:13 +0530

    Adding Recommendation if wrong word entered

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import json
+import difflib
+from difflib import get_close_matches
+
 
 data = json.load(open("data.json"))
 
@@ -6,6 +9,9 @@ def translate(word):
     word =  word.lower()
     if word in data:
         return data[word]
+    elif len (get_close_matches(word, data.keys())) > 0:
+        get_close_matches(word, data.keys(),n=6, cutoff=0.4)
+        return"Did you mean %s?" % get_close_matches(word,data.keys())[0]
     else:
         return("The word doesn't exist.")
 
